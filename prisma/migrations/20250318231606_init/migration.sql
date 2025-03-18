bf1494bbcb249bff1fcb9814174295e14b108e82
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2025-03-18 18:16:51 -0500

    update products-ms

---
+++ prisma/migrations/20250318231606_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `available` BOOLEAN NOT NULL DEFAULT true,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Product_available_idx`(`available`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
